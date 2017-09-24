ac9dc066bd6cb80f04a4b90f099661a7625e9df5
Mokhlas Hussein <mokhleshussien@aol.com>
2017-09-24 11:02:21 +0200

    workaround to fix stuck refresh on android
    render Refresh Header only if the refresh title equal to Pull to refresh

--- src/refreshableScrollView.android.js
+++ src/refreshableScrollView.android.js
@@ -181,26 +181,29 @@ export default class RefreshableScrollView extends ScrollView {
     };
 
     renderRefreshHeader() {
-        if (this.props.customRefreshView) {
+        if (this.state.refreshTitle !== "Pull to refresh") {
+            if (this.props.customRefreshView) {
+                return (
+                    <View style={[defaultHeaderStyles.header, this.props.refreshViewStyle]}>
+                        {this.props.customRefreshView(this.state.refreshStatus, this._offsetY)}
+                    </View>
+                );
+            }
+
             return (
-                <View style={[defaultHeaderStyles.header, this.props.refreshViewStyle]}>
-                    {this.props.customRefreshView(this.state.refreshStatus, this._offsetY)}
+                <View style={[defaultHeaderStyles.header, this.props.refreshViewStyle, {height: this.state.showRefreshHeader ? this.props.refreshViewHeight : 0}]}>
+                    <View style={defaultHeaderStyles.status}>
+                        {this.renderSpinner()}
+                        <Text style={defaultHeaderStyles.statusTitle}>{this.state.refreshTitle}</Text>
+                    </View>
+                    {this.props.displayDate &&
+                    <Text
+                        style={[defaultHeaderStyles.date, this.props.dateStyle]}>{this.props.dateTitle + this.state.date}</Text>
+                    }
                 </View>
             );
         }
 
-        return (
-            <View style={[defaultHeaderStyles.header, this.props.refreshViewStyle, {height: this.state.showRefreshHeader ? this.props.refreshViewHeight : 0}]}>
-                <View style={defaultHeaderStyles.status}>
-                    {this.renderSpinner()}
-                    <Text style={defaultHeaderStyles.statusTitle}>{this.state.refreshTitle}</Text>
-                </View>
-                {this.props.displayDate &&
-                <Text
-                    style={[defaultHeaderStyles.date, this.props.dateStyle]}>{this.props.dateTitle + this.state.date}</Text>
-                }
-            </View>
-        );
     }
 
     renderSpinner() {
